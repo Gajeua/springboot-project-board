7a3fa4a731d24c397b59f7d8ca0a7d75214884b6
LeeSeungHun <seung29954@gmail.com>
2022-08-17 23:12:15 +0900

    #17 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 9e1591a 에서 확인했기 때문에, 나머지 뷰도 동일하게 작동할 것으로 예상하고
    미리 테스트 메소드 업데이트

--- src/test/java/com/springboot/board/controller/ArticleControllerTest.java
+++ src/test/java/com/springboot/board/controller/ArticleControllerTest.java
@@ -48,7 +48,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk()) // 상태가 OK인지
-                .andExpect(content().contentType(MediaType.TEXT_HTML))  // 미디어 타입 지정 (화면이니 TEXT_HTML)
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))  // 미디어 타입 지정 (화면이니 TEXT_HTML)
                 .andExpect(view().name("articles/detail"))  // 매핑 될 View의 HTML에 대한 이름 테스트
                 .andExpect(model().attributeExists("article")) // 데이터가 있는지 확인
                 .andExpect(model().attributeExists("articleComments")); // 데이터가 있는지 확인
@@ -63,7 +63,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk()) // 상태가 OK인지
-                .andExpect(content().contentType(MediaType.TEXT_HTML))  // 미디어 타입 지정 (화면이니 TEXT_HTML)
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))  // 미디어 타입 지정 (화면이니 TEXT_HTML)
                 .andExpect(view().name("article/search")); // 매핑 될 View의 HTML에 대한 이름 테스트
     }
 
@@ -76,7 +76,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk()) // 상태가 OK인지
-                .andExpect(content().contentType(MediaType.TEXT_HTML))  // 미디어 타입 지정 (화면이니 TEXT_HTML)
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))  // 미디어 타입 지정 (화면이니 TEXT_HTML)
                 .andExpect(view().name("article/search-hashtag")); // 매핑 될 View의 HTML에 대한 이름 테스트
     }
 
