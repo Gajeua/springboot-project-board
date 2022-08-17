b500546a38af0c1d1d83e23ec107aa8090b936a2
LeeSeungHun <seung29954@gmail.com>
2022-08-17 22:53:11 +0900

    #17 - 게시판 뷰 기본 골격 작성
    컨트롤러가 핸들러 메서드로 테스트를 통과하게 열어줌.
    뷰 적절히 뼈대 잡음.
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했다.
    적절히 테스트 메서드 수정

--- src/main/java/com/springboot/board/controller/ArticleController.java
+++ src/main/java/com/springboot/board/controller/ArticleController.java
@@ -1,11 +1,20 @@
 package com.springboot.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles") // 매핑 url을 디자인한다.
 @Controller
 public class ArticleController {
+
+    @GetMapping("")
+    public String articles(ModelMap map){
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 
 }
--- src/test/java/com/springboot/board/controller/ArticleControllerTest.java
+++ src/test/java/com/springboot/board/controller/ArticleControllerTest.java
@@ -25,7 +25,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
+
     @DisplayName("[view] [GET] Articles List (article) page")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
@@ -34,7 +34,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk()) // 상태가 OK인지
-                .andExpect(content().contentType(MediaType.TEXT_HTML))  // 미디어 타입 지정 (화면이니 TEXT_HTML)
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))  // 미디어 타입 지정 (화면이니 TEXT_HTML)
                 .andExpect(view().name("articles/index"))  // 매핑 될 View의 HTML에 대한 이름 테스트
                 .andExpect(model().attributeExists("articles")); // 데이터가 있는지 확인
     }
